8cc432a9e393d6a2afb77ae8a959a4d63563d05f
Louis Medina <louismedina@Louiss-MacBook-Pro.local>
2018-02-23 08:50:21 -0500

    Initial Commit

---
+++ solution-code/models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema   = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: String,
+  password: String,
+  name: String,
+  familyName: String,
+  city: {type: String, default: "Madrid"},
+  role: {
+    type: String,
+    enum: ['Boss', 'Developer', 'Teacher Assistant', 'Student'],
+    default: 'Student'
+  }
+}, {
+  timestamps: { createdAt: "created_at", updatedAt: "updated_at" }
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
+// name: String, familyName: String, city: String
